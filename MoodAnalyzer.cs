34a9a8ac39188329bb19d3ba4f36e526098c3fd7
bhumika1111 <bhoyarpatil@434>
2022-08-19 23:30:14 +0530

    *Check Message Given I Am In Any Mood*

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MoodAnalyzerProb
+{
+   public class MoodAnalyzer
+    {
+        private string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        //Analysing the mood by passing I am in any mood.
+        public string CheckMood(string message)
+        {
+            if (this.message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
