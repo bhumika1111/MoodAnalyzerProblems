5fb8d720885df6c95cec8f62d539414cdd99ae97
bhumika1111 <bhoyarpatil@434>
2022-08-19 23:12:18 +0530

    *Check Message Given I Am In Sad Mood*

---
+++ MoodAnalyze.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+   public class MoodAnalyze
+    {
+        private string message;
+        public MoodAnalyze(string message)
+        {
+            this.message = message;
+        }
+        //Analysing the mood by passing happy or sad.
+        public string CheckMood(string message)
+        {
+            if (this.message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+
+    }
+}
