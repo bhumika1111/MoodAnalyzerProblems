db6f03ff0c77c8872bca1ec10d3ba07391c7294e
bhumika1111 <bhoyarpatil@434>
2022-08-21 22:05:27 +0530

    *Added MoodAnalyzer Refractor Program File*

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerRefractor
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood(string message)
+        {
+            if (this.message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
